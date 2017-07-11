1f9611e3c8bad254cccdab308712c7ee3f8fa808
Ruben Duran <rd@rubenduran.net>
2017-07-10 21:00:20 -0700

    initial commit

---
+++ BucketList/AddItemTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddItemTableViewController.swift
+//  BucketList
+//
+//  Created by Ruben Duran on 7/10/17.
+//  Copyright © 2017 RD. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: NSObject {
+
+}
---
+++ BucketList/AddItemTableViewControllerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddItemTableViewControllerDelegate.swift
+//  BucketList
+//
+//  Created by Ruben Duran on 7/10/17.
+//  Copyright © 2017 RD. All rights reserved.
+//
+
+import Foundation
